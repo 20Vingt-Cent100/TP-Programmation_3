e050641e77668b3ee9a08f3dcbbae2f2dcf8c034
Vincent <vincent.robert.31416@gmail.com>
2025-11-14 13:53:00 -0500

    Nouvelle version de Journal.java

--- src/main/java/ca/qc/bdeb/sim/tp2_camelotavelo/Journal.java
+++ src/main/java/ca/qc/bdeb/sim/tp2_camelotavelo/Journal.java
@@ -1,37 +1,46 @@
 package ca.qc.bdeb.sim.tp2_camelotavelo;
 
+import javafx.geometry.Point2D;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
 import java.util.Random;
 
 public class Journal extends GameObject implements Gravity{
-    private final Image SPRITE = new Image("assets/journal.png");
     private static float mass;
 
-    public Journal(double posX, double posY) {
-        super(posX, posY, 52, 31);
+    private Image sprite = new Image("ca/qc/bdeb/sim/tp2_secondVersion/assets/journal.png");
+
+    private Point2D acceleration;
+    private Point2D speed;
+
+    public Journal(double posX, double posY, double width, double height, Point2D speed, Point2D initialMomentum) {
+        super(posX, posY, width, height);
+        this.acceleration = new Point2D(0,0);
+        this.speed = speed.add(initialMomentum.multiply(1/mass));
     }
 
     @Override
     protected void update() {
+        position = position.add(speed.multiply(Time.deltaTimeSec));
+        speed = speed.add(acceleration.multiply(Time.deltaTimeSec));
 
+        if(position.getY() < 0)
+            this.delete();
     }
 
     @Override
-    protected void draw() {
-
-    }
-
-    public static void reloadMass(){
-
-    }
-
-    public static void CreateJournal(){
-
+    protected void draw(GraphicsContext graphicsContext, Camera camera) {
+        graphicsContext.drawImage(sprite, position.getX() - camera.getX(), graphicsContext.getCanvas().getHeight() -(size.getY()+position.getY()));
     }
 
     @Override
     public void applyGravity() {
-
+        acceleration = acceleration.add(gravity.multiply(Time.deltaTimeSec));
     }
+
+    public static void setMass(){
+        mass = new Random().nextFloat(1, 2);
+    }
+
 }
