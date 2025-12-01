d21e7797bb44abc75c0b70747cdb77048c1914de
Vincent Robert <volcanic.lavioo@gmail.com>
2025-11-30 22:32:47 -0500

    Commentaire classe Time

--- src/main/java/ca/qc/bdeb/sim/tp2_camelotavelo/Time.java
+++ src/main/java/ca/qc/bdeb/sim/tp2_camelotavelo/Time.java
@@ -4,10 +4,18 @@ public class Time {
     public static double deltaTimeSec;
     public static long deltaTimeNano;
 
+    //Temps de début de la dernière frame
     private static long pastTime;
 
+    /**
+     * Génère la valeur initiale de pastTime (Juste avant de démarrer la boucle de jeu)
+     */
     public static void loadPastTime(){pastTime = System.nanoTime();}
 
+    /**
+     * Génère la différence entre le pastTime et now
+     * @param now le temps actuelle de jeu
+     */
     public static void deltaTime(long now){
         deltaTimeNano = now - pastTime;
         deltaTimeSec = deltaTimeNano / 1_000_000_000.;
